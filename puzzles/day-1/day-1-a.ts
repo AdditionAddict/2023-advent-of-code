e243e163c8049f3c6fc4044f7ff2fa105e66f61d
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2023-12-01 14:50:00 +0000

    day-1-a

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -3,7 +3,22 @@ import chalk from 'chalk';
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let answer = 0;
+
+  const regex1 = /\D?(\d{1}).*(\d{1})\D?/;
+  const regex2 = /\D?(\d{1})\D?/;
+
+  for (let i = 0; i < data.length; i++) {
+    const matches = data[i].match(regex1) ?? data[i].match(regex2);
+    const dig1 = matches[1];
+    const dig2 = matches[2] ?? matches[1];
+
+    console.log(`${dig1}${dig2}`);
+    answer += +`${dig1}${dig2}`;
+  }
+
+  return answer;
 }
 
 const answer = await day1a();
