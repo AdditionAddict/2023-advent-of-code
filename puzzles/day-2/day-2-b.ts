fb5435c4f78721dd472cda2435f83b6652e42884
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2023-12-02 10:04:10 +0000

    day-2-b

--- puzzles/day-2/day-2-b.ts
+++ puzzles/day-2/day-2-b.ts
@@ -1,9 +1,30 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const regexRed = /\b(\d+) red\b/g;
+const regexGreen = /\b(\d+) green\b/g;
+const regexBlue = /\b(\d+) blue\b/g;
+
 export async function day2b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let sumPower = 0;
+
+  for (let game of data) {
+    const gameId = +game.match(/Game (\d+).*/)[1];
+
+    const redPicked = [...game.matchAll(regexRed)].map((m) => +m[1]);
+    const greenPicked = [...game.matchAll(regexGreen)].map((m) => +m[1]);
+    const bluePicked = [...game.matchAll(regexBlue)].map((m) => +m[1]);
+
+    const maxRedPicked = Math.max(...redPicked);
+    const maxGreenPicked = Math.max(...greenPicked);
+    const maxBluePicked = Math.max(...bluePicked);
+
+    sumPower += maxRedPicked * maxGreenPicked * maxBluePicked;
+  }
+
+  return sumPower;
 }
 
 const answer = await day2b();
