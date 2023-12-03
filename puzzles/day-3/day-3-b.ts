0d3f0ebab3b1191a12c64da9c590f8834589d288
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2023-12-03 09:43:48 +0000

    day-3-b

--- puzzles/day-3/day-3-b.ts
+++ puzzles/day-3/day-3-b.ts
@@ -3,7 +3,60 @@ import chalk from 'chalk';
 
 export async function day3b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let gearRatioSum = 0;
+
+  // each number location starts at some (a, b)
+  const partNumberStart = new Map<string, number>();
+  const partNumberStarts = new Set<string>();
+  // map other parts of the number to the start
+  // (a,b) -> (a,b), (a, b+1) -> (a,b) etc.
+  const partLookup = new Map<string, string>();
+
+  for (let [i, line] of data.entries()) {
+    // get part numbers and locations
+    for (let match of line.matchAll(/\b(\d+)\b/g)) {
+      const j = match.index;
+      const part = match[1];
+
+      // save starting location
+      partNumberStart.set(`${i}_${j}`, +part);
+
+      // save lookups to start
+      for (let k = 0; k < part.length; k++) {
+        partLookup.set(`${i}_${j + k}`, `${i}_${j}`);
+      }
+    }
+  }
+
+  for (let [i, line] of data.entries()) {
+    //  symbol locations
+    for (let match of line.matchAll(/([\*])/g)) {
+      const j = match.index;
+
+      const linkedTo = [
+        `${i - 1}_${j - 1}`,
+        `${i - 1}_${j}`,
+        `${i - 1}_${j + 1}`,
+        `${i}_${j - 1}`,
+        `${i}_${j + 1}`,
+        `${i + 1}_${j - 1}`,
+        `${i + 1}_${j}`,
+        `${i + 1}_${j + 1}`,
+      ]
+        .filter((l) => partLookup.has(l))
+        .map((l) => partLookup.get(l));
+
+      const uniquelyLinkedTo = [...new Set(linkedTo)];
+
+      if (uniquelyLinkedTo.length === 2)
+        gearRatioSum +=
+          partNumberStart.get(uniquelyLinkedTo[0]) *
+          partNumberStart.get(uniquelyLinkedTo[1]);
+    }
+  }
+
+  return gearRatioSum;
 }
 
 const answer = await day3b();
