8e5567f71e64cb72e4d8474d78392bfc6e6f93c4
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2023-12-04 05:39:21 +0000

    day-4-a

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -3,7 +3,23 @@ import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let totalScore = 0;
+
+  for (let line of data) {
+    const game = line.split(': ')[1].split(' | ');
+    const wCard = game[0].split(' ').filter((s) => s !== '');
+    const aCard = game[1].split(' ').filter((s) => s !== '');
+
+    const winningNums = new Set(wCard);
+
+    const matchingCount = aCard.filter((a) => winningNums.has(a)).length;
+    const score = matchingCount > 0 ? Math.pow(2, matchingCount - 1) : 0;
+
+    totalScore += score;
+  }
+
+  return totalScore;
 }
 
 const answer = await day4a();
