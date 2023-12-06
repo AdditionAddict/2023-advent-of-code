bb5dfcc75a9c3eeac85907d29092629e8f228dcd
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2023-12-06 13:33:52 +0000

    day-6-b

--- puzzles/day-6/day-6-b.ts
+++ puzzles/day-6/day-6-b.ts
@@ -3,7 +3,26 @@ import chalk from 'chalk';
 
 export async function day6b(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let count = 0;
+
+  const time = +data[0]
+    .split(':')[1]
+    .split(' ')
+    .filter((s) => s.length > 0)
+    .join('');
+  const distance = +data[1]
+    .split(':')[1]
+    .split(' ')
+    .filter((s) => s.length > 0)
+    .join('');
+
+  for (let h = 0; h <= time; h++) {
+    const distanceCalc = h * (time - h);
+    if (distanceCalc > distance) count++;
+  }
+
+  return count;
 }
 
 const answer = await day6b();
