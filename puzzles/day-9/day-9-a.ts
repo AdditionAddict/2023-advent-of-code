ff0c2163258c59e4d8208963db63de8379dc3e4b
Andrew Allen <andrew@Andrews-MacBook-Air.local>
2023-12-10 18:53:14 +0000

    day-9-a

--- puzzles/day-9/day-9-a.ts
+++ puzzles/day-9/day-9-a.ts
@@ -3,7 +3,42 @@ import chalk from 'chalk';
 
 export async function day9a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+
+  let totalInterpolated = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    let sequence = [...data[i].matchAll(/\b(\d+)\b/g)].map((m) =>
+      m.index > 0 && m.input[m.index - 1] === '-' ? -1.0 * +m[1] : +m[1]
+    );
+
+    const lastValues = generateDifferencesUntilAllZero(sequence);
+
+    totalInterpolated += lastValues.reduce((a, b) => a + b, 0);
+  }
+
+  return totalInterpolated;
+}
+
+function generateDifferences(nums: number[]) {
+  let differences = [];
+
+  for (let i = 1; i < nums.length; i++) {
+    differences.push(nums[i] - nums[i - 1]);
+  }
+
+  return differences;
+}
+
+function generateDifferencesUntilAllZero(nums: number[]) {
+  let lastValues = [nums[nums.length - 1]];
+  let differences = generateDifferences(nums);
+
+  while (differences.some((d) => d !== 0)) {
+    lastValues.push(differences[differences.length - 1]);
+    differences = generateDifferences(differences);
+  }
+
+  return lastValues;
 }
 
 const answer = await day9a();
